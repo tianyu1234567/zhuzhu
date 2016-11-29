87946faf09fd1f6f9f92ff82e74e9727238627b9
tianyu1234567 <wuxiaoqian123@126.com>
2016-11-29 10:23:32 +0800

    dd

---
+++ js/js.js
@@ -0,0 +1,98 @@
+$(function(){
+   
+    $(".meau").click(function(){
+    
+      if($(".nav").css("left")!=="0px"){
+            $('.nav').animate({'left':'0'},200);
+            $(".meau").removeClass("db").addClass("dn");
+           
+        }
+          
+        
+    });
+    $(".nav_gb").click(function(){
+          $('.nav').animate({'left':'-5.1rem'},200);
+            $(".meau").removeClass("dn").addClass("db");
+    });
+    
+   
+    /*touch事件*/
+   /* var box = document.querySelector("body");
+		   var startPos,endPos,startTime,endTime;
+    
+           box.addEventListener("touchstart",function(e){
+           e = window.event || e;
+               
+			startPos = {
+				x : e.touches[0].clientX,
+				y : e.touches[0].clientY
+			}
+			startTime = new Date().getTime();
+             
+            
+		})                
+
+		box.addEventListener("touchmove",function(e){
+             e = window.event || e;
+             e.preventDefault();
+			 endPos = {
+				x : e.touches[0].clientX,
+				y : e.touches[0].clientY
+			}
+          
+		})
+
+		box.addEventListener("touchend",function(e){
+             e = window.event || e;
+            
+			endTime = new Date().getTime();
+			judgeDirection()
+             
+		})
+      
+		
+
+		function  judgeDirection(){
+			var xSpan = endPos.x - startPos.x;
+			var ySpan = endPos.y - startPos.y;
+			var direction;
+            var distance = Math.sqrt(xSpan*xSpan+ySpan*ySpan);
+			if(xSpan<0&&Math.abs(ySpan/xSpan)<1&&endTime- startTime<300&&distance>100){
+				
+             
+             $('.nav').animate({'left':'-5.1rem'},200);
+             $(".meau").addClass("db").removeClass("dn")
+               
+                
+			}
+			if(xSpan>0&&Math.abs(ySpan/xSpan)<1&&endTime- startTime<300&&distance>100){
+				
+               
+            $('.nav').animate({'left':'0'},200);
+            $(".meau").addClass("dn").removeClass("db")
+           
+			}
+		
+		};
+    
+    */
+ /*回到顶部*/
+$(".index_hddb").click(function(){
+    $("body").animate({"scrollTop":"0"},200)
+});
+/*meau菜单二级*/
+
+$(".index_meauul").find("li").find("h2").click(function(){
+   
+    if($(this).siblings().hasClass("db")){
+        $(this).siblings().removeClass("db").addClass("dn")
+    }else{
+        
+    
+     $(this).siblings().removeClass("dn").addClass("db").parent().siblings("li").find("div").removeClass("db").addClass("dn");
+    }
+    
+})
+
+})
+
